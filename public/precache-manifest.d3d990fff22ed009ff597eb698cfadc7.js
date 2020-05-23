327cbec11c418a0a6df3a6456330ec2021790eb6
nomadjesse93 <jesse93@protonmail.com>
2020-05-22 18:24:02 -0600

    build

---
+++ public/precache-manifest.d3d990fff22ed009ff597eb698cfadc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8ebf5c8f09f5557f6f97d695a4d99d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d91cd74e8bfc4f00198",
+    "url": "/static/css/main.ddb9dcbd.chunk.css"
+  },
+  {
+    "revision": "759e2c853fc65d92d8f5",
+    "url": "/static/js/2.3e0bc11d.chunk.js"
+  },
+  {
+    "revision": "f72bab3bd4a02dab15fd6483b08c05f6",
+    "url": "/static/js/2.3e0bc11d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d91cd74e8bfc4f00198",
+    "url": "/static/js/main.f0e1e3d2.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "928ace9e3072f1950dee617374cbb042",
+    "url": "/static/media/home_image.928ace9e.jpg"
+  }
+]);
